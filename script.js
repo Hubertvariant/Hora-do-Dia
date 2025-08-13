f7b648171af6a91ae386a5dd4a7441ce2764e866
Hubertvariant <hubert.ramos@escola.pr.gov.br>
2025-08-13 07:42:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,16 +5,17 @@ var res = window.document.querySelector('p#msgh')
 var img = window.document.querySelector('img#imagem')
 if (hora <= 12 && hora >= 5) {
     window.document.body.style.background = '#f1e3b1ff'
-    res.innerHTML = `Agora são ${hora} Horas, Bom Dia!`
+    res.innerHTML = `Agora são ${hora} Horas. Bom Dia!`
     img.src = 'manha.jpg'
 }else if (hora <= 18 && hora > 12) {
     window.document.body.style.background = '#dc9b6f'
-     res.innerHTML = `Agora são ${hora} Horas, Boa Tarde!`
+     res.innerHTML = `Agora são ${hora} Horas. Boa Tarde!`
      img.src = 'tarde.jpg'
 }else {
     window.document.body.style.background = '#1c4377ff'
-     res.innerHTML = `Agora são ${hora} Horas, Boa Noite!`
+     res.innerHTML = `Agora são ${hora} Horas. Boa Noite!`
      img.src = 'noite.jpg'
 }
 
 }
+
